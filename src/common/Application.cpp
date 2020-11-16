669084036b64a592a5dfa48c6ad13b62c91e48d9
vsensu <vsensu@foxmail.com>
2020-11-16 12:13:07 +0800

    add xmake support

--- src/common/Application.cpp
+++ src/common/Application.cpp
@@ -4,15 +4,15 @@
 
 #include "Application.h"
 
+#include <string>
 #include <iostream>
 #include <chrono>
-#include <thread>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <imgui.h>
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
@@ -21,6 +21,39 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 constexpr double g_update_freq = 16.6 / 1000;
+
+namespace OpenGL
+{
+    void init(int major, int minor)
+    {
+        // glfw: initialize and configure
+        // ------------------------------
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    }
+
+    GLFWwindow* SetupWindow(int width, int height, const std::string &title, GLFWframebuffersizefun)
+    {
+        // glfw window creation
+        // --------------------
+        GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, title.c_str(), nullptr, nullptr);
+        if (window == nullptr)
+        {
+            std::cout << "Failed to create GLFW window" << std::endl;
+            glfwTerminate();
+            return nullptr;
+        }
+        glfwMakeContextCurrent(window);
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+        return window;
+    }
+}
 
 Application::Application()
 : Application(0, nullptr)
---
+++ xmake.lua
@@ -0,0 +1,20 @@
+add_requires("conan::glfw/3.3.2", {alias = "glfw"})
+add_requires("conan::glm/0.9.9.8", {alias = "glm"})
+add_requires("conan::imgui/1.78", {alias = "imgui"})
+
+target("samui")
+    set_kind("binary")
+    set_languages("c99", "c++20")
+    add_includedirs("deps/glad/include")
+    add_includedirs("deps/imgui/platforms/include")
+    add_files("deps/**/*.cpp")
+    add_files("deps/**/*.c")
+    add_files("src/**/*.cpp")
+    add_files("src/*.cpp")
+    if is_plat("windows") then 
+        -- set_policy("check.auto_ignore_flags", false)
+        add_ldflags("-subsystem:windows", "-entry:mainCRTStartup", {force = true}) 
+        add_links("user32", "gdi32", "shell32")
+    -- add_links("user32", "gdi32", "shell32", "vcruntime", "msvcrt")
+    end
+    add_packages("glfw", "glm", "imgui")
