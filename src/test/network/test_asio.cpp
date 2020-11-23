9e653dbbb95baaf774c8cff7c3b28a24c1be3e20
vsensu <vsensu@foxmail.com>
2020-11-23 20:31:05 +0800

    add asio test

--- src/test/network/test_asio.cpp
+++ src/test/network/test_asio.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 
+#ifdef _WIN32
+#define _WIN32_WINNT 0X0A00
+#endif
 #define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
 
 int main()
 {
-    std::cout << "test\n";
+    asio::error_code ec;
+
+    asio::io_context context;
+
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+
+    asio::ip::tcp::socket socket(context);
+
+    socket.connect(endpoint, ec);
+
+    if(!ec)
+    {
+        std::cout << "Connected!\n";
+    }
+    else
+    {
+        std::cout << "Failed to connect to address:\n" << ec.message() << "\n";
+    }
+
     return 0;
 }
--- src/test/network/xmake.lua
+++ src/test/network/xmake.lua
@@ -1,3 +1,6 @@
+add_requires("conan::asio/1.18.0", {alias = "asio"})
+
 target("test_asio")
     set_kind("binary")
     add_files("test_asio.cpp")
+    add_packages("asio")
