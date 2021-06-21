49262a5883c8cb6774d6387ea8b614f87b42c90e
vsensu <vsensu@foxmail.com>
2021-06-21 14:36:56 +0800

    add assertion

--- src/core/core.h
+++ src/core/core.h
@@ -11,6 +11,19 @@
 #else
   #error samui only support windows!
 #endif
+
+#ifdef SAMUI_DEBUG
+  #define SAMUI_ENABLE_ASSERTS
+#endif
+
+#ifdef SAMUI_ENABLE_ASSERTS
+  #define SAMUI_ASSERT(x, ...) { if(!(x)) {SAMUI_ERROR("Assertion Failed: {0}", __VA_ARGS__);} }
+  #define SAMUI_ENGINE_ASSERT(x, ...) { if(!(x)) {SAMUI_ENGINE_ERROR("Assertion Failed: {0}", __VA_ARGS__);} }
+#else
+  #define SAMUI_ASSERT(x, ...)
+  #define SAMUI_ENGINE_ASSERT(x, ...)
+#endif
+
 // clang-format on
 
 #define BIT(x) (1 << x)
--- src/xmake.lua
+++ src/xmake.lua
@@ -1,10 +1,16 @@
-add_defines("SAMUI_BUILD_DLL")
-
 add_requires("spdlog 1.8.5")
 add_requires("glfw 3.3.2")
 
 -- includes("log", "events")
 -- add_requires("conan::imgui/1.78", {alias = "imgui"})
+
+add_defines("SAMUI_BUILD_DLL")
+
+if is_mode("debug") then
+    add_defines("SAMUI_DEBUG")
+elseif is_mode("release") then
+    add_defines("SAMUI_RELEASE")
+end
 
 target("engine")
     set_kind("shared")
