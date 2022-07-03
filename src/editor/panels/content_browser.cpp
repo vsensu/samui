8f99d6c0ffec937b3b5cefe697339a7acd24412c
vsensu <vsensu@foxmail.com>
2022-07-03 23:38:43 +0800

    Fix crash

--- src/editor/panels/content_browser.cpp
+++ src/editor/panels/content_browser.cpp
@@ -41,37 +41,40 @@ void ContentBrowser::OnImGuiRender()
 
     ImGui::Columns(column_count, 0, false);
 
-    // todo: path_ not exists cause crash
-    for (const auto& dir_entry : std::filesystem::directory_iterator(path_))
+    if (std::filesystem::exists(path_))
     {
-        const auto& path = dir_entry.path().filename();
+        for (const auto& dir_entry : std::filesystem::directory_iterator(path_))
+        {
+            const auto& path = dir_entry.path().filename();
 
-        ImGui::PushID(path.string().c_str());
-        std::shared_ptr<Texture2D> icon =
-            dir_entry.is_directory() ? folder_icon_ : file_icon_;
-        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.0f, 0.0f, 0.0f, 0.0f));
-        ImGui::ImageButton((ImTextureID)icon->get_texture_id(),
-                           ImVec2(thumbnail_size, thumbnail_size), {0, 1},
-                           {1, 0});
-        if (ImGui::BeginDragDropSource())
-        {
-            auto abs_path = std::filesystem::absolute(dir_entry.path());
-            const wchar_t* path = abs_path.c_str();
-            ImGui::SetDragDropPayload("CONTENT_BROWSER_ITEM", path,
-                                      (wcslen(path) + 1) * sizeof(wchar_t));
-            ImGui::EndDragDropSource();
-        }
-        ImGui::PopStyleColor();
-        if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(0))
-        {
-            if (dir_entry.is_directory())
+            ImGui::PushID(path.string().c_str());
+            std::shared_ptr<Texture2D> icon =
+                dir_entry.is_directory() ? folder_icon_ : file_icon_;
+            ImGui::PushStyleColor(ImGuiCol_Button,
+                                  ImVec4(0.0f, 0.0f, 0.0f, 0.0f));
+            ImGui::ImageButton((ImTextureID)icon->get_texture_id(),
+                               ImVec2(thumbnail_size, thumbnail_size), {0, 1},
+                               {1, 0});
+            if (ImGui::BeginDragDropSource())
             {
-                path_ /= path;
+                auto abs_path = std::filesystem::absolute(dir_entry.path());
+                const wchar_t* path = abs_path.c_str();
+                ImGui::SetDragDropPayload("CONTENT_BROWSER_ITEM", path,
+                                          (wcslen(path) + 1) * sizeof(wchar_t));
+                ImGui::EndDragDropSource();
             }
+            ImGui::PopStyleColor();
+            if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(0))
+            {
+                if (dir_entry.is_directory())
+                {
+                    path_ /= path;
+                }
+            }
+            ImGui::TextWrapped(path.string().c_str());
+            ImGui::NextColumn();
+            ImGui::PopID();
         }
-        ImGui::TextWrapped(path.string().c_str());
-        ImGui::NextColumn();
-        ImGui::PopID();
     }
 
     ImGui::Columns(1);
